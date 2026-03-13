3388f1b6881c241bcebb92ce43b5894f6920edf0
Bitttu4 <patelarya432006@gmail.com>
2026-03-13 22:12:29 +0530

    Update practical23.java

--- practical23.java
+++ practical23.java
@@ -1,7 +1,32 @@
 // Write a program that defines a Circle class with two constructors. The  first  form  accepts  a  double  value  that represents the radius of the circle. The second form accepts the integer radius of the circle and calculates the area of the circle
 class circle{
-    
-};
+    double radius;
+    circle(double r){
+        radius = r;
+    }
+    circle(int r){
+        radius = r;
+    }
+    double area(){
+        return Math.PI * radius * radius;
+    }
+    double circumference(){
+        return 2 * Math.PI * radius;
+    }
+    void display(){
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + area());
+        System.out.println("Circumference: " + circumference());
+    }
+}
 public class practical23 {
-    
+    public static void main(String[] args) {
+        circle c1 = new circle(5.5);
+        circle c2 = new circle(7);
+        System.out.println("Circle 1:");
+        c1.display();
+        System.out.println("\nCircle 2:");
+        c2.display();
+    }
+        
 }
